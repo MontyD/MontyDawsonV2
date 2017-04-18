5cf486f371dc67748ff5e3ee4ba147de719645fe
montyD <billy-da-fish-3rd@hotmail.com>
2017-04-18 21:12:47 +0100

    Refine keyboard nav test

--- src/components/keyboard-nav.test.ts
+++ src/components/keyboard-nav.test.ts
@@ -1,27 +1,14 @@
 import {} from 'jest';
 import KeyboardNav from './keyboard-nav';
 
-let eventListeners = [];
-
-const attachEvent = (evnt: string, elem: any, func: Function) => {
-    eventListeners.push({
-        evnt,
-        elem,
-        func
-    });
-};
+const attachEvent = jest.fn();
 
 describe('KeyboardNav', () => {
 
-    beforeEach(() => {
-        eventListeners = [];
-    });
-
     it('will attach keydown to the window method', () => {
-        
+
         new KeyboardNav([], attachEvent);
-        expect(eventListeners.length).toBe(1);
-        expect(eventListeners[0].evnt).toEqual('keydown');
+        expect(attachEvent).toHaveBeenCalledWith('keydown', window, expect.any(Function));
 
     });
 
