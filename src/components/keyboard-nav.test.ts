c67b20696321f19385cb1763ffd14fcb22e27809
montyD <billy-da-fish-3rd@hotmail.com>
2017-04-19 22:57:21 +0100

    Added tests to keyboard-nav

--- src/components/keyboard-nav.test.ts
+++ src/components/keyboard-nav.test.ts
@@ -1,15 +1,56 @@
 import {} from 'jest';
 import KeyboardNav from './keyboard-nav';
+import { attachEvent } from '../dom';
+let registeredCallback: Function;
 
-const attachEvent = jest.fn();
+const mockFunction = jest.fn();
+const mockedAttachEvent = (event: string, windowObj: object, callback: Function) => {
+    registeredCallback = callback;
+};
+const testBindings = {
+    key: 0,
+    heights: [0, 100000],
+    prevent: false,
+    fn: jest.fn()
+};
+const mockedKeyDownEvent = <KeyboardEvent> {
+    keyCode: testBindings.key
+};
 
 describe('KeyboardNav', () => {
 
+    beforeEach(() => {
+        registeredCallback = null;
+        testBindings.fn.mockClear();
+    });
+
     it('will attach keydown to the window method', () => {
 
-        new KeyboardNav([], attachEvent);
-        expect(attachEvent).toHaveBeenCalledWith('keydown', window, expect.any(Function));
+        new KeyboardNav([], mockFunction);
+        expect(mockFunction).toHaveBeenCalledWith('keydown', window, expect.any(Function));
 
+    });
+
+    it('will call the event passed in on keypress - mocked', () => {
+        const mockedKeyDownEvent = <KeyboardEvent> {
+            keyCode: testBindings.key
+        };
+        new KeyboardNav([testBindings], mockedAttachEvent);
+        // This simulates calling the event listener
+        registeredCallback(mockedKeyDownEvent);
+
+        expect(testBindings.fn).toHaveBeenCalled();
+    });
+
+    it('will call the event passed in on keypress - through DOM', () => {
+        // actually attach event to the DOM
+        new KeyboardNav([testBindings], attachEvent);
+        let keyboardEvent = new KeyboardEvent('keydown', {
+            bubbles : true, cancelable : true, key : '1', shiftKey : false,
+        });
+        document.dispatchEvent(keyboardEvent);
+
+        expect(testBindings.fn).toHaveBeenCalled();
     });
 
 
