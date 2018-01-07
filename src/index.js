70959b5de6b15af311c99d7837e45582832c536c
montyD <hello@montydawson.co.uk>
2018-01-07 21:58:36 +0000

    Role back and add CV

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import style from '../style/index.scss';
-import DescriptionCrawler from './components/description-crawler.js';
 import ContextMenu from './components/context-menu.js';
 import ViewHeightFix from './components/view-height-fix.js';
-import KeyboardNav from './components/keyboard-nav.js';
 import OfflinePlugin from 'offline-plugin/runtime';
 
 (() => {
 
     'use strict';
-
-    const crawler = new DescriptionCrawler(document.querySelector('p'));
 
     const contextMenu = new ContextMenu([{
       name: 'View Source',
@@ -20,13 +16,6 @@ import OfflinePlugin from 'offline-plugin/runtime';
     }]);
 
     const viewHeightFix = new ViewHeightFix(['header', 'main', 'footer']);
-
-    const keyboardNavigation = new KeyboardNav([{
-        key: [9, 13, 37, 39],
-        fn: crawler.crawlNext.bind(crawler),
-        prevent: true,
-        heights: [0, '50vh']
-      }]);
 
     const offline = OfflinePlugin.install();
 
